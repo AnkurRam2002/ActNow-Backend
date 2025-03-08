d283d20deb8092e0b8f1370d01cf17451fc002a3
ayuxi03 <ayushimoitra03@gmail.com>
2025-03-09 00:09:49 +0530

    Added User and Event schemas

---
+++ models/event.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+
+const eventSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    require: true
+  },
+  date: {
+    type: Date,
+    required: true
+  },
+  location: {
+    type: String,
+    required: true
+  },
+  requiredSkills: {
+    type: [String]
+  },
+  volunteersNeeded: {
+      type: Number,
+      required: true
+    },
+  volunteersAssigned: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }],
+  organizer: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  status: {
+    type: String,
+    enum: ['Upcoming', 'Ongoing', 'Completed'],
+    default: 'Upcoming'
+  },
+
+}, { timestamps: true });
+
+module.exports = mongoose.model('Event', eventSchema);
