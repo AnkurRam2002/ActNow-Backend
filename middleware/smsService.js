7f1880665e7d167811858e9ad4958074b7eeae38
BitasMaiti <bitasmaiti@gmail.com>
2025-03-21 19:57:47 +0530

    modified sms service

--- middleware/smsService.js
+++ middleware/smsService.js
@@ -1,25 +1,30 @@
 require('dotenv').config();
-  const twilio = require('twilio');
-  
-  // Twilio client initialization
-  const twilioClient = twilio(
-    process.env.TWILIO_ACCOUNT_SID,  
-    process.env.TWILIO_AUTH_TOKEN    
-  );
-  
-  // Function to send SMS
-  const sendSMS = async (to, text) => {
-    try {
-      const message = await twilioClient.messages.create({
-        body: `ActNow Customer Support Team: ${text}` ,
-        to,
-        from: process.env.TWILIO_PHONE_NUMBER  
-      });
-  
-      console.log("SMS sent: ", message.sid);
-    } catch (error) {
-      console.error("Error sending SMS: ", error);
+const twilio = require('twilio');
+
+// Twilio client initialization
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const twilioClient = twilio(accountSid, authToken);
+
+// Function to send SMS
+const sendSMS = async (to, text) => {
+  try {
+    // Format the phone number with +91 prefix if it doesn't already start with +
+    let formattedNumber = to;
+    if (!to.startsWith('+')) {
+      formattedNumber = `+91${to}`;
     }
-  };
-  
-  module.exports = sendSMS;
+
+    const message = await twilioClient.messages.create({
+      body: `ActNow Customer Support Team: ${text}`,
+      to: formattedNumber,
+      from: '+19206333652'
+    });
+
+    console.log("SMS sent: ", message.sid);
+  } catch (error) {
+    console.error("Error sending SMS: ", error);
+  }
+};
+
+module.exports = sendSMS;
