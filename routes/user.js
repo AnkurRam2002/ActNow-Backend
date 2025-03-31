a712f1f401b7a1402100d2a3c6dd28f541a38685
ayuxi03 <ayushimoitra03@gmail.com>
2025-03-31 11:07:23 +0530

    Added logic for fetching user details by id

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,22 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/user.models');
 
+//Get user details by id -> new change
+router.get('/:id', async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id)
+      .populate('eventsRegistered', 'name date location') // Fetch event details
+      .populate('eventsCompleted', 'name date location')
+      .populate('eventsCreated', 'name date location');
+
+    if (!user) return res.status(404).json({ error: 'User not found' });
+
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to fetch user details.' });
+  }
+});
+
 // Get registered events
 router.get('/:id/eventsRegistered', async (req, res) => {
   try {
