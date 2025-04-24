2881c9940f5a58e3f0c76b742bdc822251e7010d
AnkurRam2002 <ankurram2002@gmail.com>
2025-04-24 14:06:03 +0530

    added get all user route

--- routes/user.js
+++ routes/user.js
@@ -190,4 +190,14 @@ router.delete('/:id', auth, async (req, res) => {
   }
 });
 
+router.get('/', async (req, res) => {
+ try{
+    const users = await User.find().populate('eventsRegistered', 'name date location').populate('eventsCompleted', 'name date location').populate('eventsCreated', 'name date location');
+      res.status(200).json(users);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+});
+
+
 module.exports = router;
