f9b117f6e421166fef5a47bfe2643e18728dbb46
ayuxi03 <ayushimoitra03@gmail.com>
2025-04-29 11:26:11 +0530

    admin role added to user model

--- models/user.models.js
+++ models/user.models.js
@@ -17,7 +17,7 @@ const userSchema = new mongoose.Schema({
   },
   role: {
     type: String,
-    enum: ['volunteer', 'ngo'],
+    enum: ['volunteer', 'ngo', 'admin'],
     default: 'volunteer'
   },
   phoneNumber: {
